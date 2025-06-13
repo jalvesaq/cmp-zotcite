9d57c74649423b22a9dd931cc03d420e9a7f710a
Jakson Alves de Aquino <jalvesaq@gmail.com>
2025-06-12 22:44:39 -0300

    Format code with stylua

--- lua/cmp_zotcite/init.lua
+++ lua/cmp_zotcite/init.lua
@@ -1,55 +1,44 @@
 local cmp = require("cmp")
 local zc = require("zotcite.config")
 
-local source = { }
+local source = {}
 
-local options = { filetypes = {'markdown', 'rmd', 'quarto', 'vimwiki'}}
+local options = { filetypes = { "markdown", "rmd", "quarto", "vimwiki" } }
 
 source.new = function()
     local self = setmetatable({}, { __index = source })
     return self
 end
 
-source.setup = function(opts)
-    options = vim.tbl_extend('force', options, opts or {})
-end
+source.setup = function(opts) options = vim.tbl_extend("force", options, opts or {}) end
 
-source.get_debug_name = function()
-    return 'cmp_zotcite'
-end
+source.get_debug_name = function() return "cmp_zotcite" end
 
 source.is_available = function()
     for _, v in pairs(options.filetypes) do
-        if vim.bo.filetype == v then
-            return true
-        end
+        if vim.bo.filetype == v then return true end
     end
     return false
 end
 
 source.resolve = function(_, completion_item, callback)
-    if not completion_item.textEdit then
-        return callback(completion_item)
-    end
+    vim.schedule(require("zotcite.config").hl_citations)
+    if not completion_item.textEdit then return callback(completion_item) end
     local zkey = string.gsub(completion_item.textEdit.newText, "#.*", "")
     local ref = vim.fn.py3eval('ZotCite.GetRefData("' .. zkey .. '")')
     if ref then
         local doc = ""
         local ttl = " "
-        if ref.title then
-            ttl = ref.title
-        end
+        if ref.title then ttl = ref.title end
         local etype = string.gsub(ref.etype, "([A-Z])", " %1")
         etype = string.lower(etype)
         doc = etype .. "\n\n**" .. ttl .. "**\n\n"
         if ref.etype == "journalArticle" and ref.publicationTitle then
-                doc = doc .. "*" .. ref.publicationTitle .. "*\n\n"
+            doc = doc .. "*" .. ref.publicationTitle .. "*\n\n"
         elseif ref.etype == "bookSection" and ref.bookTitle then
             doc = doc .. "In *" .. ref.bookTitle .. "*\n\n"
         end
-        if ref.alastnm then
-            doc = doc .. ref.alastnm
-        end
+        if ref.alastnm then doc = doc .. ref.alastnm end
         if ref.year then
             doc = doc .. " (" .. ref.year .. ") "
         else
@@ -57,23 +46,20 @@ source.resolve = function(_, completion_item, callback)
         end
         completion_item.documentation = {
             kind = cmp.lsp.MarkupKind.Markdown,
-            value =  doc
+            value = doc,
         }
     end
     callback(completion_item)
 end
 
-
 source.complete = function(_, request, callback)
     local charb = string.sub(request.context.cursor_before_line, request.offset - 1)
-    if string.sub(charb, 1, 1) ~= '@' then
-        return {}
-    end
+    if string.sub(charb, 1, 1) ~= "@" then return {} end
 
     local lines = vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, true)
 
     -- Check if we are within normal markdown text
-    local lnum = vim.fn.line('.')
+    local lnum = vim.fn.line(".")
     local ismd = true
     for i = lnum, 1, -1 do
         if string.find(lines[i], "^```{") then
@@ -85,7 +71,10 @@ source.complete = function(_, request, callback)
                 -- after a code block
                 break
             else
-                if (string.find(lines[i], "^---$") or string.find(lines[i], "^%.%.%.$")) and i > 1 then
+                if
+                    (string.find(lines[i], "^---$") or string.find(lines[i], "^%.%.%.$"))
+                    and i > 1
+                then
                     -- after YAML front matter
                     break
                 else
@@ -99,9 +88,7 @@ source.complete = function(_, request, callback)
         end
     end
 
-    if not ismd then
-        return {}
-    end
+    if not ismd then return {} end
 
     local resp = {}
     local input = string.sub(request.context.cursor_before_line, request.offset)
@@ -113,7 +100,7 @@ source.complete = function(_, request, callback)
             line = request.context.cursor.line,
             character = request.offset - 1,
         },
-        ['end'] = {
+        ["end"] = {
             line = request.context.cursor.line,
             character = request.context.cursor.character,
         },
@@ -121,11 +108,12 @@ source.complete = function(_, request, callback)
 
     -- Get matching Zotero keys
     local fullfname = vim.fn.expand("%:p")
-    if vim.fn.has('win32') == 1 then
-        fullfname = string.gsub(tostring(fullfname), '\\', '/')
+    if vim.fn.has("win32") == 1 then
+        fullfname = string.gsub(tostring(fullfname), "\\", "/")
     end
     if not zc.inited() then zc.init() end
-    local itms = vim.fn.py3eval('ZotCite.GetMatch("' .. input .. '", "' .. fullfname .. '")')
+    local itms =
+        vim.fn.py3eval('ZotCite.GetMatch("' .. input .. '", "' .. fullfname .. '")')
     if itms then
         for _, v in pairs(itms) do
             local txt = v[2] .. " " .. v[3]
@@ -137,12 +125,17 @@ source.complete = function(_, request, callback)
                 kind = cmp.lsp.CompletionItemKind.Reference,
                 textEdit = {
                     newText = v[1],
-                    range = text_edit_range
-                }
+                    range = text_edit_range,
+                },
             })
         end
     end
     callback({ items = resp })
+end
+
+source.execute = function(_, item, cb)
+    vim.schedule(require("zotcite.config").hl_citations)
+    cb(item)
 end
 
 return source
